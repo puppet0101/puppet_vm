b1a31a1f580a5ab3a248edaddb38f90d2e62f330
hunt <hunt@linuxmaster.com>
2015-10-15 18:55:29 +0900

    Added sshd module to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,8 @@ node 'losttemple.linuxmaster.com' {
 node 'vm01.linuxmaster.com' {
     file { '/tmp/testerver':
         content => "This is a test server.\n",
-    {
+    }
+    include sshd
 }
 
 node /^vm[0-9]+\.linuxmaster\.com$/ {
